c5841208dbf0c0b6abb15a48524fc15863b7c66b
ouahabi1M <mo1ouahabi1@gmail.com>
2023-04-10 19:49:17 -0700

    first commit

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _abs - computes the absolute value of an integer
  * @n: the int to check
@@ -6,9 +7,9 @@
  */
 int _abs(int n)
 {
-if (n >= 0)
-{
-return (n);
-}
-return (-n);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -7,16 +8,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int l = 0;
-int x = 0;
-while (*(src + l) != '\0')
-{
-l++;
-}
-for ( ; x < l ; x++)
-{
-dest[x] = src[x];
-}
-dest[l] = '\0';
-return (dest);
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
