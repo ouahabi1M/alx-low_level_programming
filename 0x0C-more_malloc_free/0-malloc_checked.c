0aaf6b066cb44676815fede246ac16c6671326b1
ouahabi1M <mo1ouahabi1@gmail.com>
2023-04-13 16:55:06 -0700

    first commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,20 @@
+#include <stdlib.h>
 #include "main.h"
 
-
 /**
-* malloc_checked - cause normal process termination with a status value of 98
-*
-* @b: allocated memory
-*
-* Return: a pointer to the allocated memory
-*/
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-   void *i;
+	void *ptr;
 
+	ptr = malloc(b);
 
-   i = malloc(b);
-   if (i == NULL)
-       exit(98);
-   return (i);
-} 
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
